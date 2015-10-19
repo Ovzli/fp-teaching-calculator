16adfc55dcf7b7ec68c3841492e0ef6259726a09
Brian Pierson <14piersonb@gmail.com>
2015-10-18 20:24:36 -0400

    Creates ability to parse hex string to a linked list of characters

--- fp-teaching-calculator/src/edu/cs222/fp_teaching_calculator/model/converter/InputParser.java
+++ fp-teaching-calculator/src/edu/cs222/fp_teaching_calculator/model/converter/InputParser.java
@@ -1,7 +1,24 @@
 package edu.cs222.fp_teaching_calculator.model.converter;
 
+import java.util.LinkedList;
+
 public class InputParser {
 	//	Contributors = 
 	//	Responsibility = 
 
+	public LinkedList<Character> parseHexidecimalInput(String input){
+		LinkedList<Character> characters = new LinkedList<Character>();//rename characters?
+		for(int i = 0; i < input.length() ; i++){
+			characters.add(input.charAt(i));
+			}
+ 		return characters;		
+	}
+	
+	public LinkedList<Character> parseDecimalInput(String input){
+		return null;		
+	}
+	
+	public LinkedList<Character> parseBinary(String input){
+		return null;		
+	}
 }
--- fp-teaching-calculator/src/edu/cs222/fp_teaching_calculator/model/converter/InputValidator.java
+++ fp-teaching-calculator/src/edu/cs222/fp_teaching_calculator/model/converter/InputValidator.java
@@ -4,5 +4,6 @@ public class InputValidator {
 	//	Contributors = HexToBinConvertor
 	//	Responsibility = Validates each character of the
 	//			user's input string to verify it is a hex value
-
+	
+	public boolean hexadecimalValidator()
 }
