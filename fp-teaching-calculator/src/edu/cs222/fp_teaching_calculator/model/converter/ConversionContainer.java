de065f812adeabf3014cc61746df86e3dd70405a
Brian Pierson <14piersonb@gmail.com>
2015-10-21 17:32:34 -0400

    Actually pushes new classes
    See prior commit

---
+++ fp-teaching-calculator/src/edu/cs222/fp_teaching_calculator/model/converter/ConversionContainer.java
@@ -0,0 +1,70 @@
+package edu.cs222.fp_teaching_calculator.model.converter;
+
+import java.util.LinkedList;
+
+public class ConversionContainer {
+	public final int errorCode;	//boolean or int??
+	public final String originalInput;
+	public final LinkedList<String> parsedListOfHexInput; 
+	public final LinkedList<String> listOfDecimalEquivalents; 
+	public final LinkedList<String> listOfBinaryEquivalents;
+	public final LinkedList<int[]>  listOfSeparatedBinaryNibbles;
+
+	public ConversionContainer(int errorCode){
+		this.errorCode = errorCode;
+		this.originalInput = null;
+		this.parsedListOfHexInput = null;
+		this.listOfBinaryEquivalents = null;
+		this.listOfDecimalEquivalents = null;
+		this.listOfSeparatedBinaryNibbles = null;
+	}
+	
+	public ConversionContainer(ContainerBuilder builder){
+		this.errorCode = -1;
+		this.originalInput = builder.originalInput;
+		this.parsedListOfHexInput = builder.parsedListOfHexInput;
+		this.listOfBinaryEquivalents = builder.listOfBinaryEquivalents;
+		this.listOfDecimalEquivalents = builder.listOfDecimalEquivalents;
+		this.listOfSeparatedBinaryNibbles = builder.listOfSeparatedBinaryNibbles;
+	}
+	
+	public static class ContainerBuilder{
+		
+		public String originalInput;
+		public LinkedList<String> parsedListOfHexInput; 
+		public LinkedList<String> listOfDecimalEquivalents; 
+		public LinkedList<String> listOfBinaryEquivalents;
+		public LinkedList<int[]>  listOfSeparatedBinaryNibbles;
+		
+		public ContainerBuilder originalInput(String input){
+			this.originalInput = input;
+			return this;
+		}
+		
+		public ContainerBuilder parsedListOfHexInput(LinkedList<String> listOfHexInput){
+			this.parsedListOfHexInput = listOfHexInput;
+			return this;
+		}
+		
+		public ContainerBuilder listOfDecimalEquivalents(LinkedList<String> listOfDecEquivalents){
+			this.listOfDecimalEquivalents = listOfDecEquivalents;
+			return this;
+		}
+		
+		public ContainerBuilder listOfBinaryEquivalents(LinkedList<String> listOfBinEquivalents){
+			this.listOfBinaryEquivalents = listOfBinEquivalents;
+			return this;
+		}
+		
+		public ContainerBuilder listOfSeparatedBinaryNibbles(LinkedList<int[]> listOfNibbles){
+			this.listOfSeparatedBinaryNibbles = listOfNibbles;
+			return this;
+		}
+		
+		public ConversionContainer build(){
+			return new ConversionContainer(this);
+		}
+		
+		
+	}
+}
