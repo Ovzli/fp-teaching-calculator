19516f6449d38663ce430b30ab6d8ce55ef636d2
Deric <Deric@10.0.76.62>
2015-11-08 14:28:21 -0500

    Revise Value Translator to eliminate predefined lists.

--- fp-teaching-calculator/src/edu/cs222/fpteachingcalculator/model/converter/ValueTranslator.java
+++ fp-teaching-calculator/src/edu/cs222/fpteachingcalculator/model/converter/ValueTranslator.java
@@ -3,15 +3,10 @@ package edu.cs222.fpteachingcalculator.model.converter;
 import java.util.LinkedList;
 
 public class ValueTranslator {
-	private String[] allHexSymbolsSorted = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
-	private String[] allDecValuesSorted = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
-	private String[] allBinValuesSorted = {"0000","0001","0010","0011","0100","0101","0110","0111","1000","1001","1010",
-											"1011","1100","1101","1110","1111"};
-
 	public LinkedList<String> translateHexToDec(LinkedList<String> listOfHexSymbols) {
 		LinkedList<String> decValues = new LinkedList<String>();
 		for (String hexSymbol : listOfHexSymbols) {
-			decValues.add(allDecValuesSorted[getPostitionOfValue(allHexSymbolsSorted, hexSymbol)]);
+			decValues.add( Integer.toString( Integer.parseInt(hexSymbol, 16) )  );
 		}
 		return decValues;
 	}
@@ -19,7 +14,7 @@ public class ValueTranslator {
 	public LinkedList<String> translateDecToBin(LinkedList<String> listOfDecValues) {
 		LinkedList<String> binValues = new LinkedList<String>();
 		for (String decValue : listOfDecValues) {
-			binValues.add(allBinValuesSorted[getPostitionOfValue(allDecValuesSorted, decValue)]);
+			binValues.add(Integer.toBinaryString(Integer.parseInt(decValue,10)));
 		}
 		return binValues;
 	}
