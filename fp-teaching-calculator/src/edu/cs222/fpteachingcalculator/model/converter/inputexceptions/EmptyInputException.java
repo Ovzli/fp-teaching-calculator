26c9f02b04131b1a8930ca536403141de4567ec7
Brian Pierson <14piersonb@gmail.com>
2015-11-23 10:08:16 -0500

    Update exception serialVersionUIDs to have generated values

--- fp-teaching-calculator/src/edu/cs222/fpteachingcalculator/model/converter/inputexceptions/EmptyInputException.java
+++ fp-teaching-calculator/src/edu/cs222/fpteachingcalculator/model/converter/inputexceptions/EmptyInputException.java
@@ -2,7 +2,11 @@ package edu.cs222.fpteachingcalculator.model.converter.inputexceptions;
 
 public class EmptyInputException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4094429946745483017L;
 
 	public EmptyInputException() {
 	}
