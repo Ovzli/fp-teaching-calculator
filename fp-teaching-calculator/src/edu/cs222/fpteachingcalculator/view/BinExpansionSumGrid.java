ee993f9217c47e2fcffbc6bf4e245fc20ea3da64
Brian Pierson <14piersonb@gmail.com>
2015-12-11 07:38:21 -0500

    Add files I forgot

---
+++ fp-teaching-calculator/src/edu/cs222/fpteachingcalculator/view/BinExpansionSumGrid.java
@@ -0,0 +1,26 @@
+package edu.cs222.fpteachingcalculator.view;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.scene.layout.GridPane;
+
+public class BinExpansionSumGrid extends GridPane {
+	private final List<BigValueExpansion> bigValueBinaryList = new LinkedList<>();
+	private final List<BigValueExpansion> bigValueSumExpansionList = new LinkedList<>();
+
+	public BinExpansionSumGrid(List<List<Integer>> binDigits) {
+		super();
+		this.setVgap(5);
+		for (int i = 0; i < binDigits.size(); i++) {
+			bigValueBinaryList.add(new BigValueExpansion(binDigits.get(i)));
+			this.add(bigValueBinaryList.get(i), 0, i);
+			this.add(new BigCharLabel("="), 1, i);
+			bigValueSumExpansionList.add(new BigValueExpansion(binDigits.get(i)));
+			this.add(bigValueSumExpansionList.get(i), 2, i);
+			bigValueSumExpansionList.get(i).addParenthesis(8);
+			bigValueSumExpansionList.get(i).addPlusSigns(3);
+			bigValueSumExpansionList.get(i).multiplyByExpansion();
+		}
+	}
+}
---
+++ fp-teaching-calculator/src/edu/cs222/fpteachingcalculator/view/DecSumGrid.java
@@ -0,0 +1,24 @@
+package edu.cs222.fpteachingcalculator.view;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javafx.scene.layout.GridPane;
+
+public class DecSumGrid extends GridPane {
+	private final List<BigValueExpansion> bigValueSumExpansionList = new LinkedList<>();
+
+	public DecSumGrid(List<List<Integer>> binDigits, List<String> decValues) {
+		super();
+		this.setVgap(5);
+		for (int i = 0; i < binDigits.size(); i++) {
+			bigValueSumExpansionList.add(new BigValueExpansion(binDigits.get(i)));
+			this.add(bigValueSumExpansionList.get(i), 0, i);
+			bigValueSumExpansionList.get(i).addParenthesis(8);
+			bigValueSumExpansionList.get(i).addPlusSigns(3);
+			bigValueSumExpansionList.get(i).multiplyByExpansion();
+			this.add(new BigCharLabel("="), 1, i);
+			this.add(new BigCharLabel(decValues.get(i)), 2, i);
+		}
+	}
+}
